5a3e8f2b3851342bb0ad1e84df8a433700aac9ec
hemantDwivedi <hemantdwiwedi@gmail.com>
2023-12-23 20:54:28 +0530

    initializing project

---
+++ src/test/java/com/javaclt/openaijavaclient/OpenaiJavaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaclt.openaijavaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenaiJavaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
